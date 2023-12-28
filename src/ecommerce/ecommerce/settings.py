718632921f3437f2ab6b55d88ea7444e8ce7c698
Nathian Leticia <netosarmento@gmail.com>
2023-12-28 12:22:15 -0300

    lesson6

--- src/ecommerce/ecommerce/settings.py
+++ src/ecommerce/ecommerce/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "static_local")
 ]
 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
