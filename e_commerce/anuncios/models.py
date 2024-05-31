fbdc690c35d764a9940350f3dcecaeaa79aed0a2
netosarmento <netosarmento@gmail.com>
2024-05-31 10:01:01 -0300

    ajeitando os templates2

--- e_commerce/anuncios/models.py
+++ e_commerce/anuncios/models.py
@@ -1,21 +1,23 @@
 from django.db import models
-
-from django.db import models
-
-
+from django.utils.text import slugify
 
 class Categoria(models.Model):
     titulo = models.CharField(max_length=40)
+    slug = models.SlugField(unique=True)  # Add the slug field
 
     def __str__(self):
         return self.titulo
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.titulo)  # Generate the slug do titulo
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ['titulo']
 
-
 class Anuncio(models.Model):
     titulo = models.CharField(max_length=40)
+    slug = models.SlugField(unique=True)  # Add the slug field
     imagem = models.ImageField(upload_to='static/img/')
     descricao = models.TextField(null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
--- e_commerce/anuncios/views.py
+++ e_commerce/anuncios/views.py
@@ -1,39 +1,19 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
-
-from .models import Categoria
-from .models import Anuncio
-
+# views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Categoria, Anuncio
 
 def postagens(request):
     categorias = Categoria.objects.all()
-
     ultimos_anuncios = Anuncio.objects.all()[:12]
+    return render(request, 'postagens.html', {'categorias': categorias, 'anuncios': ultimos_anuncios})
 
-    return render(request, 'postagens.html', {'categorias': categorias,
-                                         'anuncios': ultimos_anuncios})
-
-def categoria(request, categoria_id):
+def categoria(request, categoria_id): # Adicione o parâmetro categoria_id
     categoria = get_object_or_404(Categoria, id=categoria_id)
-
     categorias = Categoria.objects.all()
-
     anuncios = Anuncio.objects.filter(categoria=categoria)
+    return render(request, 'anuncio.html', {'categorias': categorias, 'anuncios': anuncios, 'categoria': categoria})
 
-    return render(request, 'home.html', {'categorias': categorias,
-                                         'anuncios': anuncios,
-                                         'categoria': categoria})
-
-
-def anuncio(request, anuncio_id):
-    anuncio = get_object_or_404(Anuncio, id=anuncio_id)
-
+def anuncio(request, anuncio_id):  # Adicione o parâmetro anuncio_id
+    anuncio_obj = get_object_or_404(Anuncio, id=anuncio_id)  # Use anuncio_id para recuperar o anúncio
     categorias = Categoria.objects.all()
-    
-
-    return render(request, 'anuncio.html', {'categorias': categorias,
-                                         'anuncio': anuncio})
-
+    return render(request, 'anuncio.html', {'categorias': categorias, 'anuncio': anuncio_obj})
