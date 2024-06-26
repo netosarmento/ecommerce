2bbfc85cf0c2cc4ce6af4b79b20b1fa72591e060
Carlos Jorge Sarmento Neto <119957570+netosarmento@users.noreply.github.com>
2024-06-26 01:24:50 -0300

    Update models.py

--- e_commerce/anuncios/models.py
+++ e_commerce/anuncios/models.py
@@ -3,13 +3,13 @@ from django.utils.text import slugify
 
 class Categoria(models.Model):
     titulo = models.CharField(max_length=40)
-    slug = models.SlugField(unique=True)  # Add the slug field
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.titulo
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.titulo)  # Generate the slug do titulo
+        self.slug = slugify(self.titulo)
         super().save(*args, **kwargs)
 
     class Meta:
@@ -17,8 +17,7 @@ class Categoria(models.Model):
 
 class Anuncio(models.Model):
     titulo = models.CharField(max_length=40)
-    slug = models.SlugField(unique=True)  # Add the slug field
-    imagem = models.ImageField(upload_to='static/img/')
+    slug = models.SlugField(unique=True)
     descricao = models.TextField(null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     criado_em = models.DateTimeField(auto_now_add=True)
@@ -29,3 +28,15 @@ class Anuncio(models.Model):
 
     class Meta:
         ordering = ['-id']
+
+class Midia(models.Model):
+    TIPO_CHOICES = (
+        ('imagem', 'Imagem'),
+        ('video', 'Vídeo'),
+    )
+    anuncio = models.ForeignKey(Anuncio, related_name='midias', on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    url = models.URLField()
+
+    def __str__(self):
+        return f"{self.tipo} - {self.url}"
